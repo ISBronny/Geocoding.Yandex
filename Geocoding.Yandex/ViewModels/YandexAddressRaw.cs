1401cfc273b5ea4bfd85900a3063fff864e8bbc9
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-04 10:34:35 +0300

    Postal Code

--- Geocoding.Yandex/ViewModels/YandexAddressRaw.cs
+++ Geocoding.Yandex/ViewModels/YandexAddressRaw.cs
@@ -6,6 +6,7 @@ namespace Geocoding.Yandex.ViewModels
     internal class YandexAddressRaw 
     {
         [JsonProperty("country_code")] public string CountryCode { get; set; }
+        [JsonProperty("postal_code")] public string PostalCode { get; set; }
 
         [JsonProperty("formatted")] public string Formatted { get; set; }
 

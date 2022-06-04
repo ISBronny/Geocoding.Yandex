bb0af43eb3d447bcdd2ddd4a5295b9a10bc8f552
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-04 10:34:44 +0300

    PremiseNumber

--- Geocoding.Yandex/ViewModels/YandexPremise.cs
+++ Geocoding.Yandex/ViewModels/YandexPremise.cs
@@ -5,5 +5,6 @@ namespace Geocoding.Yandex.ViewModels
     internal class YandexPremise
     {
         [JsonProperty("PremiseName")] public string PremiseName { get; set; }
+        [JsonProperty("PremiseNumber")] public string PremiseNumber { get; set; }
     }
 }

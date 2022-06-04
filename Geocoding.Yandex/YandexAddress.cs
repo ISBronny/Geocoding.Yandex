8bb88d48b924a0e3fb10ddf28dd42473c5695ec7
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-04 10:35:34 +0300

    YandexAddress - static factory

--- Geocoding.Yandex/YandexAddress.cs
+++ Geocoding.Yandex/YandexAddress.cs
@@ -1,13 +1,50 @@
-﻿namespace Geocoding.Yandex
+﻿using System.Globalization;
+using Geocoding.Yandex.ViewModels;
+
+namespace Geocoding.Yandex
 {
-
-// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
-
-    public class YandexAddress : Address
+    public class YandexAddress : ParsedAddress
     {
-        public YandexAddress(string formattedAddress, Location coordinates, string provider) : base(formattedAddress, coordinates, provider)
+        public string HouseNumber { get; set; }
+
+        private YandexAddress(string formattedAddress, Location coordinates, string provider) : base(formattedAddress, coordinates, provider)
         {
             
+        }
+
+        internal static YandexAddress FromGeoObject(YandexGeoObject geoObject)
+        {
+            var address = new YandexAddress(
+                geoObject.MetaDataProperty.GeocoderMetaData.Text,
+                new Location(
+                    double.Parse(geoObject.Point.Pos.Split()[1], CultureInfo.InvariantCulture),
+                    double.Parse(geoObject.Point.Pos.Split()[0], CultureInfo.InvariantCulture)
+                ),
+                "Yandex"
+            );
+
+            var country = geoObject?.MetaDataProperty?.GeocoderMetaData?.AddressDetails?.Country;
+            address.Country = country?.CountryName;
+            address.State = country?.AdministrativeArea?.AdministrativeAreaName;
+            Locality locality;
+            if (country?.AdministrativeArea?.SubAdministrativeArea == null)
+            {
+                locality = country?.AdministrativeArea?.Locality;
+                address.County = country?.AdministrativeArea?.AdministrativeAreaName;
+            }
+            else
+            {
+                locality = country?.AdministrativeArea?.SubAdministrativeArea?.Locality;
+                address.County = country?.AdministrativeArea?.SubAdministrativeArea?.SubAdministrativeAreaName;
+            }
+
+            address.City = locality?.LocalityName;
+            address.Street = locality?.Thoroughfare?.ThoroughfareName;
+            address.HouseNumber = locality?.Thoroughfare?.Premise?.PremiseNumber;
+
+            address.PostCode = geoObject?.MetaDataProperty?.GeocoderMetaData?.Address?.PostalCode;
+            
+            return address;
         }
     }
 }

2977aa1a0b900c2ab537b02ad62f196d2cec4b5c
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-04 10:36:35 +0300

    Url Generator

---
+++ Geocoding.Yandex/UrlGenerator.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Text;
+using Geocoding.Yandex.Enums;
+
+namespace Geocoding.Yandex
+{
+    internal class UrlGenerator
+    {
+        public UrlGenerator(string baseUrl, string apiKey)
+        {
+            BaseUrl = baseUrl;
+            ApiKey = apiKey;
+        }
+
+        private string ApiKey { get; }
+        private string BaseUrl { get; }
+        public string GenerateGeocodeUrl(string geocode, Bounds bounds = null, Language? language = null, int? skip = null, int? results = null)
+        {
+            var builder = GenerateBaseUrl(geocode, bounds: bounds, language: language, skip: skip, results: results);
+            return builder.ToString();
+        }
+        
+        public string GenerateGeocodeReverseUrl(string geocode, Kind? kind = null, Bounds bounds = null, Language? language = null, int? skip = null, int? results = null)
+        {
+            var builder = GenerateBaseUrl(geocode, bounds: bounds, language: language, skip: skip, results: results);
+            if (kind.HasValue)
+            {
+                builder.Append("&kind=");
+                switch (kind.Value)
+                {
+                    case Enums.Kind.District:
+                        builder.Append(WebUtility.UrlEncode("district"));
+                        break;
+                    case Enums.Kind.House:
+                        builder.Append(WebUtility.UrlEncode("house"));
+                        break;
+                    case Enums.Kind.Street:
+                        builder.Append(WebUtility.UrlEncode("street"));
+                        break;
+                    case Enums.Kind.Metro:
+                        builder.Append(WebUtility.UrlEncode("metro"));
+                        break;
+                    case Enums.Kind.Locality:
+                        builder.Append(WebUtility.UrlEncode("locality"));
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            return builder.ToString();
+        }
+
+        private StringBuilder GenerateBaseUrl(string geocode, Bounds bounds = null, Language? language = null, int? skip = null, int? results = null)
+        {
+            var builder = new StringBuilder();
+            
+            builder.Append($"{BaseUrl}?geocode={WebUtility.UrlEncode(geocode)}&format=json");
+            builder.Append($"&apikey={ApiKey}");
+            
+            if (language.HasValue)
+            {
+                builder.Append("&lang=");
+                switch (language.Value)
+                {
+                    case Enums.Language.Russian:
+                        builder.Append(WebUtility.UrlEncode("ru_RU"));
+                        break;
+                    case Enums.Language.Belarusian:
+                        builder.Append(WebUtility.UrlEncode("be_BY"));
+                        break;
+                    case Enums.Language.Ukrainian:
+                        builder.Append(WebUtility.UrlEncode("uk_UA"));
+                        break;
+                    case Enums.Language.Turkish:
+                        builder.Append(WebUtility.UrlEncode("tr_TR"));
+                        break;
+                    case Enums.Language.EnglishRussianFeatures:
+                        builder.Append(WebUtility.UrlEncode("en_RU"));
+                        break;
+                    case Enums.Language.EnglishAmericanFeatures:
+                        builder.Append(WebUtility.UrlEncode("en_US"));
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            if (bounds != null)
+            {
+                builder.Append("&rspn=1&bbox=");
+                builder.Append(
+                    $"{bounds.SouthWest.Longitude.ToString(CultureInfo.InvariantCulture)}," +
+                    $"{bounds.SouthWest.Latitude.ToString(CultureInfo.InvariantCulture)}~" +
+                    $"{bounds.NorthEast.Longitude.ToString(CultureInfo.InvariantCulture)}," +
+                    $"{bounds.NorthEast.Latitude.ToString(CultureInfo.InvariantCulture)}");
+            }
+                
+            if (skip.HasValue)
+            {
+                builder.Append($"&skip={skip.Value}");
+            }
+
+            if (results.HasValue)
+            {
+                builder.Append($"&results={results.Value}");
+            }
+
+            return builder;
+        }
+    }
+}
---
+++ Geocoding.Yandex/YandexGeocoder.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Geocoding.Yandex.Enums;
+using Geocoding.Yandex.ViewModels;
+using Newtonsoft.Json;
+
+namespace Geocoding.Yandex
+{
+    public class YandexGeocoder : IGeocoder
+    {
+        private string _apiKey;
+
+        public YandexGeocoder(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+
+        public string ApiKey
+        {
+            get { return _apiKey; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("ApiKey can not be null or empty");
+
+                _apiKey = value;
+            }
+        }
+
+        public Kind? Kind { get; set; } = null;
+        public Language? Language { get; set; } = null;
+        public int? Skip { get; set; } = null;
+        public Bounds Bounds { get; set; }
+        public int? Results { get; set; }
+        
+        async Task<IEnumerable<Address>> IGeocoder.GeocodeAsync(string address)
+        {
+            return await GeocodeAsync(address);
+        }
+
+        async Task<IEnumerable<Address>> IGeocoder.GeocodeAsync(string street, string city, string state,
+            string postalCode, string country)
+        {
+            return await GeocodeAsync(BuildAddress(street, city, state, postalCode, country));
+        }
+
+        async Task<IEnumerable<Address>> IGeocoder.ReverseGeocodeAsync(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task<IEnumerable<Address>> IGeocoder.ReverseGeocodeAsync(double latitude, double longitude)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<YandexAddress>> GeocodeAsync(string address,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (string.IsNullOrEmpty(address))
+                throw new ArgumentNullException(nameof(address));
+
+            var request = BuildWebGeocodeRequest(address);
+            return await ProcessRequest(request, cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<YandexAddress>> ReverseGeocodeAsync(Location location,  CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var request = BuildWebGeocodeReverseRequest($"{location.Longitude}, {location.Latitude}");
+            return await ProcessRequest(request, cancellationToken).ConfigureAwait(false);
+        }
+
+        private string BuildAddress(string street, string city, string state, string postalCode, string country)
+        {
+            return $"{street} {city}, {state} {postalCode}, {country}";
+        }
+
+        private async Task<IEnumerable<YandexAddress>> ProcessRequest(HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                using (var client = BuildClient())
+                {
+                    return await ProcessWebResponse(await client.SendAsync(request, cancellationToken)
+                        .ConfigureAwait(false)).ConfigureAwait(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private async Task<IEnumerable<YandexAddress>> ProcessWebResponse(HttpResponseMessage response)
+        {
+            var res = JsonConvert.DeserializeObject<YandexRoot>(await response.Content.ReadAsStringAsync());
+            var yandexGeoObjects = res.Response.GeoObjectCollection.FeatureMember.Select(x => x.GeoObject);
+            return yandexGeoObjects.Select(YandexAddress.FromGeoObject);
+        }
+
+        private HttpClient BuildClient()
+        {
+            return new HttpClient();
+        }
+
+        private HttpRequestMessage BuildWebGeocodeRequest(string value)
+        {
+            var url = new UrlGenerator("https://geocode-maps.yandex.ru/1.x", ApiKey).GenerateGeocodeUrl(value, Bounds, Language, Skip, Results);
+            return new HttpRequestMessage(HttpMethod.Get, url);
+        }
+        
+        private HttpRequestMessage BuildWebGeocodeReverseRequest(string value)
+        {
+            var url = new UrlGenerator("https://geocode-maps.yandex.ru/1.x", ApiKey).GenerateGeocodeReverseUrl(value, Kind, Bounds, Language, Skip, Results);
+            return new HttpRequestMessage(HttpMethod.Get, url);
+        }
+    }
+}
+

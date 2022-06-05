2f7adae9f4f4558557331d326646b80f65397a46
Ivan Braiko <ivan.braiko@jetbrains.com>
2022-06-05 11:54:56 +0300

    IGeocoder interface test

---
+++ Geocoding.Yandex.Tests/BaseClassGeocoderTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Geocoding.Yandex.Tests;
+
+public class BaseClassGeocoderTests : TestBase
+{
+    [Test]
+    public async Task BasicTest()
+    {
+        IGeocoder geocoder = new YandexGeocoder(ApiKey);
+        var addresses = (await geocoder.GeocodeAsync("Тверская 7")).ToList();
+        Assert.That(addresses, Is.All.Not.Null);
+    }
+    
+    [Test]
+    public async Task BasicReverseTest()
+    {
+        IGeocoder geocoder = new YandexGeocoder(ApiKey);
+        var addresses = (await geocoder.ReverseGeocodeAsync(new Location(55.743405, 37.597185))).ToList();
+        Assert.That(addresses, Is.All.Not.Null);
+    }
+}
